201726e95a736389144a9bce0f46f14993a8d133
Daniel Trejo <npm030312@gmail.com>
2024-09-06 09:23:07 -0600

    temporaly no links header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,10 +4,6 @@ export default function Header({inicio}){
     return(
     <div className={styles.Header}>
       <Link className={styles.Title} href="/">PersonalWear</Link>
-      <div className={styles.linksContainer}>
-      <Link href="/" className={styles.LinkLogin}>Ingresar</Link>
-      {inicio && <Link href="/edit" className={styles.LinkGetStarted}>Comenzar</Link>} 
-      </div>
     </div>
     )
 }
